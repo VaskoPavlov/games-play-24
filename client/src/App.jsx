58645a34207bfb4d2299849b95eda7342f5d8542
VaskoPavlov <vasko.pavlov9924@gmail.com>
2024-07-22 12:49:15 +0300

    Separate components from HTML

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
+import GameCreate from "./components/game-create/GameCreate"
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+import Login from "./components/login/Login"
+import Register from "./components/register/Register"
+
 function App() {
-  return (
-    <>
-      <h1>Games Play</h1>
-    </>
-  )
+    return (
+        <div id="box">
+            <Header />
+            <main id="main-content">
+                <Home />
+                <Login />
+                <Register />
+                <GameCreate />
+            </main>
+        </div>
+    )
 }
 
 export default App
